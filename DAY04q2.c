92fc262e62cc69d197221fdbdeb5fc976fd9e4a0
Aakarshit16 <Aakarshit.34675@stu.upes.ac.in>
2026-09-04 20:24:38 +0530

    Add files via upload

---
+++ DAY04q2.c
@@ -0,0 +1,23 @@
+/*
+* Name : Aakarshit singh 
+* Roll : 590034675
+* Day  : 04    Question: 2
+* Date : 25-08-2026 
+* 
+* PROBLEM STATEMENT: 
+* <Write a program to find and display the sum of the first n natural numbers.> 
+*/ 
+
+#include <stdio.h> 
+
+int main(void)
+{
+    int n, sum = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+     sum = sum + i;     
+    }
+    printf("The sum of the first %d natural numbers is: %d\n", n, sum);
+    return 0;
+}
